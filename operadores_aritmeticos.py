17670d4a1cf2f17290c6152d280ede46d1707377
jugfarias <jufarias13@gmail.com>
2025-07-27 20:43:34 -0300

    arquivo sobre operadores aritméticos com exemplos de cálculos e ordem de precedência das operações

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+# Soma
+print(1 + 3)
+
+# Subtração
+print(3 - 2)
+
+# Multiplicação
+print(6 * 2)
+
+# Divisão simples
+print(15 / 2)
+
+# Divisão inteira
+print(15 // 2)
+
+# Resto da divisão
+print(10 % 3)
+
+# Exponenciação
+print(2 ** 3)
