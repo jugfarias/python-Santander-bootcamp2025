b3a4d926c195da78873c3422999c4109f9aa2785
jugfarias <jufarias13@gmail.com>
2025-07-27 20:31:29 -0300

    primeiras aulas de python

---
+++ dir.py
@@ -0,0 +1,4 @@
+print('Hello world!')
+
+print(dir())
+print(dir(100))
---
+++ print.py
@@ -0,0 +1,5 @@
+nome, sobrenome = 'Ju', 'Guimarias'
+
+print(nome, sobrenome)
+print(nome, sobrenome, sep = '-')
+print(nome, sobrenome, end = ' ou será que não...? \n')
