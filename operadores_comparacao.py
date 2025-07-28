e94d466a6ee23a10c2a30fbdfa82c0ff597950e8
jugfarias <jufarias13@gmail.com>
2025-07-28 18:15:19 -0300

    operadores de comparação

---
+++ operadores_comparacao.py
@@ -0,0 +1,20 @@
+saldo = 450
+saque = 200
+
+# Igualdade
+print(saldo == saque)
+
+# Desigualdade
+print(saldo != saque)
+
+# Maior
+print(saldo > saque)
+
+# Maior ou igual 
+print(saldo >= saque)
+
+# Menor
+print(saldo < saque)
+
+# Menor ou igual
+print(saldo <= saque)
