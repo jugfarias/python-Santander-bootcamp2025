7d8a75c436564586121bf0181e19569b6ebe700f
jugfarias <jufarias13@gmail.com>
2025-07-28 18:26:53 -0300

    operadores de atribuição

---
+++ operadores_atribuicao.py
@@ -0,0 +1,31 @@
+# Atribuição simples
+a = 10  # atribui o valor 10 à variável a
+print("a =", a)
+
+# Atribuição com adição
+a += 5  # equivalente a: a = a + 5
+print("a += 5 ->", a)
+
+# Atribuição com subtração
+a -= 3  # equivalente a: a = a - 3
+print("a -= 3 ->", a)
+
+# Atribuição com multiplicação
+a *= 2  # equivalente a: a = a * 2
+print("a *= 2 ->", a)
+
+# Atribuição com divisão
+a /= 4  # equivalente a: a = a / 4
+print("a /= 4 ->", a)
+
+# Atribuição com divisão inteira
+a //= 2  # equivalente a: a = a // 2
+print("a //= 2 ->", a)
+
+# Atribuição com módulo
+a %= 3  # equivalente a: a = a % 3
+print("a %= 3 ->", a)
+
+# Atribuição com exponenciação
+a **= 4  # equivalente a: a = a ** 4
+print("a **= 4 ->", a)
