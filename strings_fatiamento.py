1b12c7de62c314dfda38f629be115439389d107d
jugfarias <jufarias13@gmail.com>
2025-08-02 12:52:49 -0300

    fatiamento de strings

---
+++ strings_fatiamento.py
@@ -0,0 +1,17 @@
+nome = 'Juliana Guimarães de Farias'
+
+print(nome[0])          # 'J'
+
+print(nome[6])          # 'a'
+
+print(nome[-1])         # 's'
+
+print(nome[:7])         # 'Juliana'
+
+print(nome[21:])        # 'Farias' 
+
+print(nome[8:18])       # 'Guimarães'
+
+print(nome[8:18:2])     # 'Giaãs'
+
+print(nome[::-1])       # 'sairaF ed seãramiuG anailuJ' 
