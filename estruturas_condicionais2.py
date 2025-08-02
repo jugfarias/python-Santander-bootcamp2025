4288197a7bc54bfae5c206639234fe71f73ff8c1
jugfarias <jufarias13@gmail.com>
2025-08-01 23:05:57 -0300

    exemplo estruturas condicionais com uso de if, elif e else

---
+++ estruturas_condicionais2.py
@@ -0,0 +1,8 @@
+opcao = int(input('Informe uma opção: \n[1] Sacar \n[2] Extrato \nEscolha: '))
+
+if opcao == 1:
+    print('Você deseja sacar dinheiro da conta.')
+elif opcao == 2:
+    print('Você deseja ver o extrato da sua conta.')
+else:
+    print('Opção inválida.')
