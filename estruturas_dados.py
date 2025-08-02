d458eed134bf50c263f94eb8b759f30a0017cedb
jugfarias <jufarias13@gmail.com>
2025-08-02 15:16:35 -0300

    exemplos simples de estruturas de dados

---
+++ estruturas_dados.py
@@ -0,0 +1,23 @@
+# Exemplos de estruturas de dados em Python
+
+# Lista
+lista = [1, 2, 3, 4, 5]
+
+# Tupla
+tupla = (10, 20, 30)
+
+# Conjunto (set)
+conjunto = {100, 200, 300}
+
+# Dicionário
+dicionario = {
+    "nome": "Ana",
+    "idade": 25,
+    "cidade": "São Paulo"
+}
+
+# Exibindo os exemplos
+print("Lista:", lista)
+print("Tupla:", tupla)
+print("Conjunto:", conjunto)
+print("Dicionário:", dicionario)
